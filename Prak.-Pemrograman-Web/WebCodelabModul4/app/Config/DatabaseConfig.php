9d65f4e277708424b7cb222b766b74c2b114f6a0
Rofiful Azhar <148960304+NafasSurya@users.noreply.github.com>
2024-12-06 06:25:05 +0700

    Add files via upload

---
+++ Prak.-Pemrograman-Web/WebCodelabModul4/app/Config/DatabaseConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Config;
+
+class DatabaseConfig
+{
+    // Database settings
+    public $host = "Localhost";
+    public $user = "root";
+    public $password = "";
+    public $database_name = "web_codelab4";
+    public $port = 3306;
+}
